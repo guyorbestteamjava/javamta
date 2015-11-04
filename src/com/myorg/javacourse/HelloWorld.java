2f38d99d56735d912891cc5006e8b89e546ede53
OrBarak1990 <barakor21@gmail.com>
2015-11-04 16:52:07 +0200

    initial

---
+++ src/com/myorg/javacourse/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.myorg.javacourse;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
